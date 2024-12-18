6581604a2780a913d54773b02b129272a42ae783
Laur401 <laurynastautvydas11@gmail.com>
2024-12-18 14:32:39 +0100

    Adding time adjustment in both directions.

--- TimeButtons.lua
+++ TimeButtons.lua
@@ -1,18 +1,35 @@
+
 if callType == LuaCallType.Init then
-    
-    
-    
-    
-    function getVariables(elementName)
-        local name, var1 = elementName.match("(.-)%[set:%s*([%l]+)%]") --<name>[set: <open/closed>]
+    function getVars(elementName)
+        api.log("Hello2!")
+        local name, var1 = elementName.match(elementName,"(.-)%[set:%s*([%l]+)%]") --<name>[set: <forward/back>]
         return {name, var1}
     end
+    buttonSetting={}
+    api.log("Hello!")
+    for i, button in pairs(g_timeButton) do
+        local element = api.getElement(button)
+        api.log(element)
+        local vars = getVars(element.elementName)
+        element.elementName=vars[1]
+        buttonSetting[i]=vars[2]
+    end
+    function getIndex(context)
+        local element = context.gameObject.GetComponent('Element')
+        local index = string.match(element.playerVariableName, "{(%d+)}")
+        return tonumber(index)
+    end
+    
 end
 
 if callType == LuaCallType.SwitchDone then
     if api.contains(g_timeButton,context) and context.isOn then
-        for i, lock in pairs(g_barLock) do
-            api.setLockValue(lock, lock.values[1]+1,1)
+        local index = getIndex(context)
+        for _, lock in pairs(g_barLock) do
+            local adjustment=0
+            if (buttonSetting[index]=="forward") and lock.values[1]<1439 then adjustment=1
+            elseif (buttonSetting[index]=="back") and lock.values[1]>0 then adjustment=-1 end
+            api.setLockValue(lock, lock.values[1]+adjustment, 1)
         end
     end
 end
